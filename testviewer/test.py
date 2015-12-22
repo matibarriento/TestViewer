f8e6df8d4caaaf8be28e66feafa6d3c6e3bc60c2
Mati Barriento <elmatibarriento@gmail.com>
2015-12-22 06:18:34 -0300

    add tests

--- testviewer/test.py
+++ testviewer/test.py
@@ -2,9 +2,24 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import print_function
+import unittest
 from model import TestRun
 from modules import trx
 
-trx_module = trx
 
-print(trx_module.ObtenerDatos("").lista_test)
+class TestModulos(unittest.TestCase):
+
+    """docstring for TestModulos"""
+
+    def test_trx_modulo(self):
+
+        path_archivo = ""
+        trx_module = trx
+        resultado = trx_module.ObtenerDatos(path_archivo).__dict__
+        print(resultado)
+        self.assertIn("nombre", resultado)
+        self.assertGreater(0, len(resultado["lista_test"]))
+
+
+if __name__ == '__main__':
+    unittest.main()
